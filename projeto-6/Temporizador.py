4b2ddd0c8b5b46d051da6d30fedde1d7b9b419de
khadidjamoraes <khadidja.developer@gmail.com>
2022-12-01 19:26:42 -0300

    Create Temporizador.py

---
+++ projeto-6/Temporizador.py
@@ -0,0 +1,12 @@
+import time
+
+iniciotempo = 60
+finaltempo = 0
+
+print ('iniciando contagem regressiva...')
+
+for i in range(iniciotempo, finaltempo, -1):
+    print (i)
+    time.sleep(1)
+
+print ('BUMMMM!!!!!!')
